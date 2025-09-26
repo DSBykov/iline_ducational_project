86f12cf538400442368cf0472fd20678d2ff7c98
Dmitry Bykov <bykov.dmitry86@yandex.ru>
2025-09-26 15:22:57 +0300

    add adding employee and filter by full name

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template, request
+from datetime import datetime, UTC
+
+from flask import Flask, render_template, request, flash, redirect, url_for
 from utils.database import db, Employee, EmployeeHierarchy, Positions
 from config import Config
 
@@ -6,6 +8,7 @@ app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = Config.SQLALCHEMY_DATABASE_URI
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['PER_PAGE'] = 10  # Количество элементов на странице
+app.config['SECRET_KEY'] = Config.SECRET_KEY
 
 db.init_app(app)
 
@@ -15,10 +18,15 @@ def employees():
     sort_by = request.args.get('sort_by', 'full_name')
     sort_order = request.args.get('sort_order', 'asc')  # Направление сортировки
     per_page = app.config['PER_PAGE']
+    search = request.args.get('search', '')
 
 
     # Базовый запрос
     query = Employee.query
+
+    # Применяем поиск
+    if search:
+        query = query.filter(Employee.full_name.ilike(f'%{search}%'))
 
     # Валидация и применение сортировки
     valid_sort_columns = [column.name for column in Employee.__table__.columns] # Допустимые поля для сортировки
@@ -27,7 +35,7 @@ def employees():
         print(sort_by, 'not in', valid_sort_columns)
         sort_by = 'full_name'  # Значение по умолчанию при невалидном поле
 
-        # Сортировка по полям Employee
+    # Сортировка по полям Employee
     column = getattr(Employee, sort_by)
     if sort_order.lower() == 'desc':
         query = query.order_by(column.desc())
@@ -80,8 +88,78 @@ def employees():
         'all_employees.html',
         employees=employees_pagination,
         sort_by=sort_by,
-        sort_order=sort_order
+        sort_order=sort_order,
+        search=search
     )
+
+
+@app.route('/add_employee/', methods=['GET', 'POST'])
+def add_employee():
+    if request.method == 'GET':
+        # Получаем список должностей и сотрудников для выпадающих списков
+        positions = Positions.query.all()
+        employees = Employee.query.options(db.joinedload(Employee.position_name)).all()
+        today = datetime.now(UTC).date()
+        return render_template('add_employee.html',
+                             positions=positions,
+                             employees=employees,
+                             today=today.isoformat())
+
+    elif request.method == 'POST':
+        try:
+            # Получаем данные из формы
+            full_name = request.form['full_name']
+            position = request.form['position_id']
+            hire_date_str = request.form.get('hire_date')
+            salary = request.form['salary']
+            boss_id = request.form.get('boss_id') or ''
+
+            # Преобразуем дату
+            hire_date = datetime.strptime(hire_date_str, '%Y-%m-%d').date()
+
+            # Валидация данных
+
+            # Создаем нового сотрудника
+            new_employee = Employee(
+                full_name=full_name,
+                position=position,
+                hire_date=hire_date,
+                salary=salary
+            )
+
+            # Записываем в БД
+            db.session.add(new_employee)
+            db.session.flush()
+
+            # Проверка, что грейд руководителя на один выше чем у подчиненного
+            new_employee_position = int(new_employee.position)
+            boss = db.session.query(Employee).filter(Employee.id == boss_id).first()
+            boss_position = int(boss.position)
+            if new_employee_position < 5:
+                if boss_position == new_employee_position + 1:
+                    # Устанавливаем связь сотрудника с руководителем
+                    hierarchy = EmployeeHierarchy(
+                        boss_id=boss_id,
+                        subordinate_id=new_employee.id
+                    )
+                    # Записываем в БД
+                    db.session.add(hierarchy)
+                else:
+                    raise ValueError('Грейд руководителя должен быть на 1 больше грейда подчиненного')
+
+            # Сохраняем изменения
+            db.session.commit()
+
+            flash('Сотрудник успешно добавлен!', 'success')
+            print('Сотрудник успешно добавлен!')
+            return redirect(url_for('employees'))
+
+        except Exception as e:
+            db.session.rollback()
+            print(f'Ошибка при добавлении сотрудника: {str(e)}', 'error')
+            flash(f'Ошибка при добавлении сотрудника: {str(e)}', 'error')
+
+    return render_template('add_employee.html')
 
 if __name__ == "__main__":
     app.run()
--- config.py
+++ config.py
@@ -1,7 +1,10 @@
 import os
+import secrets
+
 
 class Config:
-    SECRET_KEY = os.getenv('SECRET_KEY') or 'bd_password'
+    SECRET_KEY = os.getenv('SECRET_KEY', secrets.token_hex(16)) # os.getenv('SECRET_KEY') or 'bd_password'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         f'postgresql://postgres:{SECRET_KEY}@localhost:5432/iline_db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
